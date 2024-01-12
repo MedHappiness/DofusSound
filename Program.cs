a944b03f98520bbf3f32ee29f08d02bf5a91f4c4
Saad <msaad.ouss@gmail.com>
2024-01-13 00:32:06 +0100

    init

--- Program.cs
+++ Program.cs
@@ -3,22 +3,28 @@ using System.Diagnostics;
 
 string exePath = @"D:\Desktop 2.0\Argus Beta\Dofus.exe";
 string RegExeFolder = @"D:\Desktop 2.0\Argus Beta\reg\Reg.exe";
-try
-{
-    if (!SoundMain.Initialized)
-        SoundMain.Init(); // on démarre le moteur de son
 
-        Process.Start(RegExeFolder, $"--reg-engine-port={SoundMain.regServer.Port}");
-}
-catch
-{
-    Console.WriteLine("Erreur lors du lancement de Reg.exe. Les sons ne seront pas activées");
-}
+SoundMain.Init(); // on démarre le moteur de son
 
-while(true)
-{
-Process.Start(exePath, $"--lang=fr --update-server-port={SoundMain.upServer.Port} --updater_version=v2 --reg-client-port={SoundMain.gameServer.Port}");
-//Process.Start(RegExeFolder, $"--reg-engine-port={SoundMain.regServer.Port}");
+Process.Start(RegExeFolder, $"--reg-engine-port={SoundMain.regServer.Port}");
 
-Console.ReadLine();
+while (true)
+{
+    Console.WriteLine("Press D to start a new dofus instance");
+    Console.WriteLine("Press Q to quit");
+
+    var key = Console.ReadLine();
+    if (key == "D")
+    {
+        StartNewDofusInstance();
+    }
+    else if (key == "Q")
+    {
+        break;
+    }
 } // on attend que le programme se fermes pour fermer le serveur
+
+void StartNewDofusInstance()
+{
+    Process.Start(exePath, $"--lang=fr --update-server-port={SoundMain.upServer.Port} --updater_version=v2 --reg-client-port={SoundMain.gameServer.Port}");
+}
